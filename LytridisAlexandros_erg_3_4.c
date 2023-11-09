a2f78d4a873e750aba761e8cf1a4b5a96bde463f
Doodeyy <99737798+Doodeyy@users.noreply.github.com>
2023-11-09 03:10:29 +0200

    Add files via upload

--- LytridisAlexandros_erg_3_4.c
+++ LytridisAlexandros_erg_3_4.c
@@ -12,17 +12,21 @@ ARITHMOS MHTRWOY : 2023195
 
 int main() {
     int grade1, grade2, grade3, final_grade;
+
     printf("Enter grade of first grader: ");
     scanf("%d", &grade1);
     printf("Enter grade of second grader: ");
     scanf("%d", &grade2);
+
     if (abs(grade1 - grade2) < 20) {
         final_grade = (grade1 + grade2) / 2;
-    } else {
+    } 
+    else{
         printf("Enter grade of third grader: ");
         scanf("%d", &grade3);
         final_grade = (grade1 + grade2 + grade3) / 3;
     }
+
     printf("Final grade: %d/100", final_grade);
     return 0;
 }
