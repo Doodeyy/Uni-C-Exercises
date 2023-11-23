051d304bfb8bb1f88f183b35f3480e17fd9feed5
Doodeyy <99737798+Doodeyy@users.noreply.github.com>
2023-11-23 09:44:18 +0200

    Update LytridisAlexandros_erg_6_1.c

--- LytridisAlexandros_erg_6_1.c
+++ LytridisAlexandros_erg_6_1.c
@@ -1,20 +1,23 @@
-#include <stdio.h>
+
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(){
-
+    
     int i;
     int j;
-
-    printf("Give an integer in [1,10]: ");
-    scanf("%d" , &i);
-
-    for (j = 1 ; j <=10 ; j++){
-        printf("%d * %d = %d\n" , i , j , i*j);
- 
-    }
-
-    system("pause");
-    return 0;
     
-}   
+    printf("Give an integer in [1, 10]: ");
+    scanf("%d" , &i);
+    
+    while ( i < 1 || i > 10){
+        printf("Invalid input. Give an integer in [1, 10]: ");
+        scanf("%d" , &i);
+        }
+        
+    for (j = 1 ; j <= 10 ; j++){
+        printf("%dx%d=%d  " , i , j , i*j);
+        }
+        
+    system("pause");
+}
