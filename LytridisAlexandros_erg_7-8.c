9420f5fab3a09c3c0f108b7d592ccc87fbf70d6a
Doodeyy <99737798+Doodeyy@users.noreply.github.com>
2023-12-05 17:10:22 +0200

    Add files via upload

---
+++ LytridisAlexandros_erg_7-8.c
@@ -0,0 +1,179 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void fillPin(int pin[] , int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        pin[i] = rand() % 10 + 1;
+    }
+}
+
+
+void showPin(int pin[] , int n) {
+    int i;
+    for (i = 0; i < n; i++) {
+        printf("%d ", pin[i]);
+    }
+}
+
+int findThesiMax(int pin[] , int n) {
+    int max = pin[0];
+    int thesiMax = 0;
+    int i;
+    for (i = 1; i < n; i++) {
+        if (pin[i] > max) {
+            max = pin[i];
+            thesiMax = i;
+        }
+    }
+    return thesiMax;
+}
+
+int findThesiMin(int pin[] , int n) {
+    int min = pin[0];
+    int thesiMin = 0;
+    int i;
+    for (i = 1; i < n; i++) {
+        if (pin[i] < min) {
+            min = pin[i];
+            thesiMin = i;
+        }
+    }
+    return thesiMin;
+}
+
+int countAvg(int pin[] , int n) {
+    int i, mo;
+    int sum = 0;
+    int count = 0;
+    for (i = 0; i < n; i++) {
+        sum += pin[i];
+    }
+
+    mo = sum / (n);
+    
+    for (i = 0; i < n; i++) {
+        if (pin[i] > mo) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int searchPThesiNum(int pin[] , int n , int num) {
+    int i;
+    int found = -1;
+    for (i = 0; i < n; i++) {
+        if (pin[i] == num) {
+            found = i;
+            break;
+        }
+    }
+    return found;
+}
+
+void bubbleDesc(int pin[] , int n) {
+    int i , j , temp;
+    for (i = 0; i < n - 1; i++){
+        for (j = 0; j < n - i - 1; j++){
+            if (pin[j] < pin[j + 1]){
+                temp = pin[j];
+                pin[j] = pin[j + 1];
+                pin[j + 1] = temp;
+            }
+        }
+    }
+}
+
+int binarySearchPThesiNum(int pin[] , int n , int num) {
+    int found = -1;
+    int start = 0;
+    int end = n - 1;
+    int mid;
+    while (start <= end && found == -1) {
+        mid = (start + end) / 2;
+        if (pin[mid] == num) {
+         found = mid;
+        }
+        else if (pin[mid] > num) {
+            start = mid + 1;
+        }
+        else {
+            end = mid - 1;
+        }
+    }
+    return found;
+}
+
+void swapPin(int pin[] , int index) {
+    int temp;
+    temp = pin[index];
+    pin[index] = pin[index + 1];
+    pin[index + 1] = temp;
+}
+
+int main(){
+    srand(time(NULL));
+    int n , thesiMax, thesiMin;
+    
+    printf("Give an integer n > 5 : ");
+    scanf("%d" , &n);
+    while (n <= 5){
+        printf("Invalid input. Give an integer n > 5: ");
+        scanf("%d" , &n);
+    }
+    int pin[n];
+    fillPin(pin , n);
+       
+    showPin(pin , n);
+    printf("\n");
+         
+    thesiMax = findThesiMax(pin , n);
+    printf("Max = %d thesiMax = %d\n" , pin[thesiMax] , thesiMax);
+    
+    thesiMin = findThesiMin(pin , n);
+    printf("Min = %d thesiMin = %d\n" , pin[thesiMin] , thesiMin);
+
+    printf("count avg = %d\n" , countAvg(pin , n));
+
+    int num;
+    printf("Give an integer num : ");
+    scanf("%d" , &num);
+
+    int thesiNum = searchPThesiNum(pin , n , num);
+    if (thesiNum == -1) {
+        printf("NOT FOUND\n");
+    } 
+    else {
+        printf("Found num = %d in position %d, p[%d] = %d\n" , num ,  thesiNum , thesiNum , pin[thesiNum]);
+    }
+    
+    bubbleDesc(pin , n);
+
+    showPin(pin , n);
+    
+    printf("\n");
+    printf("Give an integer num : ");
+    scanf("%d" , &num);
+    
+    int thesiNum2 = binarySearchPThesiNum(pin , n , num);
+    if (thesiNum2 == -1) {
+        printf("NOT FOUND\n");
+    } 
+    else {
+        printf("Found num = %d in position %d, p[%d] = %d\n" , num ,  thesiNum2 , thesiNum2 , pin[thesiNum2]);
+    }
+
+    int index;
+    index = rand() % (n - 2);
+    printf("index = %d\n" , index);
+    
+    swapPin(pin , index);
+
+    showPin(pin , n);
+    printf("\n");
+
+    system("pause");
+    return 0;
+}
